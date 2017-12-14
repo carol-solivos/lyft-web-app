a9e07f90d386360d6808f04d721df263f52b25c7
carol-solivos <carol.solivos@gmail.com>
2017-12-14 11:27:33 -0400

    login y botones

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,18 @@ $(document).ready(function(){
   $(function(){
     setTimeout(function() {
         $('.splash').fadeOut('3000');
-    }, 3500); 
+    }, 500); //velocity in ms (3500)
   });
-  
+  $('#signUp').click(function(){
+    $('#first').hide();
+    $('#signUpSection').show();
+  });  
+  $(function(){     
+    if ($('#inputPhone').val().length < 10) {
+      $('#nextNumber').attr("disabled", true);
+    }
+    if ($('#inputPhone').val().length >= 10){
+      $('#nextNumber').removeAttr("disabled");
+    }
+  });
 });
-
